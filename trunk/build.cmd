389a953f0fd7c2188b9b1894ec7081049ab9e91c
nlaredo <nlaredo@27c12cfe-3947-0410-9e8a-8b387c640983>
2008-12-03 06:00:14 +0000

    switch from project migrated to visual studio 2008 express edition to visual studio 2005 with a non-migrated "fresh" project.  Installer target builds but doesn't install correctly... debugging required. ShellExecute was hard-coded to use ascii strings... wrap properly.

--- trunk/build.cmd
+++ trunk/build.cmd
@@ -1,7 +1,10 @@
-rem For Visual Studio 2003
-rem devenv iTunesWeb.sln /rebuild Release /project iTunesWebInstaller
-rem devenv iTunesWeb.sln /rebuild Debug
+rem For Visual Studio 2005
+rem set VS80COMNTOOLS="C:\Program Files\Microsoft Visual Studio 8\Common7\Tools\"
+call "%VS80COMNTOOLS%\vsvars32.bat"
+devenv iTunesWeb.sln /rebuild Release /project iTunesWebInstaller
+rem devenv iTunesWeb.sln /rebuild Debug /project iTunesWeb
 
+exit 0
 rem For Visual Studio Express Edition 2008
 rem set VS90COMNTOOLS=C:\Program Files\Microsoft Visual Studio 9.0\Common7\Tools\
 call "%VS90COMNTOOLS%\vsvars32.bat"
